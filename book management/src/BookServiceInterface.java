10db524b73f029471bc1df804f5713bd64280fa1
Prabin-Adhikari7 <84490427+Prabin-Adhikari7@users.noreply.github.com>
2023-01-16 19:17:51 +0545

    Add files via upload

---
+++ book management/src/BookServiceInterface.java
@@ -0,0 +1,12 @@
+public abstract class BookServiceInterface {
+
+        abstract void addBook();
+
+        abstract void showAllBooks();
+
+        abstract void showAllAvailableBooks();
+
+        abstract void borrowBook();
+
+        abstract void returnBook();
+    }
